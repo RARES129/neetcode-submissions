26199b912ab11ed455a07d3db82b3ad355c39bc0
Dascălu Rareș <94977726+RARES129@users.noreply.github.com>
2026-07-27 16:18:54 +0300

    Update: is-palindrome - submission-1

--- Data Structures & Algorithms/is-palindrome/submission-1.java
+++ Data Structures & Algorithms/is-palindrome/submission-1.java
@@ -1,20 +1,22 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        int left = 0;
-        int right = s.length()-1;
-        while(left<right){
-            if(!Character.isLetterOrDigit(s.charAt(left))){
-                left++;
-                continue;
-            } else if(!Character.isLetterOrDigit(s.charAt(right))){
-                right--;
-                continue;
-            } else if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))){
+        int i = 0;
+        int j = s.length() - 1;
+        
+        while (i < j) {
+            while (i < j && !Character.isLetterOrDigit(s.charAt(i))) {
+                i++;
+            }
+            while (i < j && !Character.isLetterOrDigit(s.charAt(j))) {
+                j--;
+            }
+            if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) {
                 return false;
             }
-            left++;
-            right--;
+            i++;
+            j--;
         }
+        
         return true;
     }
 }
