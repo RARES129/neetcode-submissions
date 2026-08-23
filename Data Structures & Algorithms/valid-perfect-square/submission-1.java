5beddb95f49c5b5bfececc67e31cdc539a57dc34
Dascălu Rareș <94977726+RARES129@users.noreply.github.com>
2026-08-23 18:31:34 +0300

    Add: valid-perfect-square - submission-1

---
+++ Data Structures & Algorithms/valid-perfect-square/submission-1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        long l=1,r=num;
+        while(l<=r){
+            long mid=l+(r-l)/2;
+            long sq = mid*mid;
+            if(sq<num){
+                l=mid+1;
+            }else if(sq>num){
+                r=mid-1;
+            }else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
