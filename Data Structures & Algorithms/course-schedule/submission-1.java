20c28bb0ecabb1b6570254de1a8b6307b7995797
Dascălu Rareș <94977726+RARES129@users.noreply.github.com>
2026-08-27 20:36:25 +0300

    Add: course-schedule - submission-1

---
+++ Data Structures & Algorithms/course-schedule/submission-1.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[] inDegree = new int[numCourses];
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i=0;i<numCourses;i++) adj.add(new ArrayList<>());
+
+        for(int[] pre: prerequisites) {
+            inDegree[pre[1]]++;
+            System.out.print(pre[0]+""+pre[1]+" ");
+            adj.get(pre[0]).add(pre[1]);
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0; i< numCourses; i++){
+            if(inDegree[i]==0){
+                q.add(i);
+            }
+        }
+
+        int finish =0;
+        while(!q.isEmpty()){
+            int node = q.poll();
+            finish++;
+            for(int nei : adj.get(node)){
+                inDegree[nei]--;
+                if(inDegree[nei]==0){
+                    q.add(nei);
+                }
+            }
+        }
+        return finish == numCourses;
+    }
+}
